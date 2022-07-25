b73e5d510d85dcac41c6641a8dc7cbebba0e2aa1
kevinjamey <67798124+kevinjamey@users.noreply.github.com>
2022-07-25 21:04:23 +0300

    fmriprep script

---
+++ code/script_fMRIPrep_Analysis.sh
@@ -0,0 +1,4 @@
+#fmriprerp
+echo $PATH
+export PATH="/Users/kevinjamey/.local/bin:$PATH"
+fmriprep-docker /Users/kevinjamey/Desktop/Nifty /Users/kevinjamey/Desktop/Nifty/derivatives participant --participant_label 16 --fs-license-file license.txt
